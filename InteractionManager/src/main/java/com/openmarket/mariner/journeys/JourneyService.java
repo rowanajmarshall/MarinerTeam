b8392aead82d82b42965894f399434a01697939b
Harrys <HarryS@GitBucket.com>
2017-08-30 15:59:24 +0100

    journey service class

--- InteractionManager/src/main/java/com/openmarket/mariner/journeys/JourneyService.java
+++ InteractionManager/src/main/java/com/openmarket/mariner/journeys/JourneyService.java
@@ -1,5 +1,20 @@
 package com.openmarket.mariner.journeys;
 
-public class JourneyService {
+import javax.ws.rs.client.WebTarget;
 
+public class JourneyService {
+    WebTarget baseUri;
+
+    public JourneyService(WebTarget webTarget) {
+        baseUri = webTarget;
+    }
+
+    public Journey getJourney(String from, String to) {
+        return baseUri.path("journey")
+                      .queryParam("from", from)
+                      .queryParam("to", to)
+                      .request()
+                      .get()
+                      .readEntity(Journey.class);
+    }
 }
